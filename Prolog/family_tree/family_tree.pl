ce1a74d1e1c1668bd9bed23658db068a55ae0e0d
Jasmin91 <Jasmin.Profus@yahoo.de>
2018-09-05 15:26:36 -0700

    hw 1

--- Prolog/family_tree/family_tree.pl
+++ Prolog/family_tree/family_tree.pl
@@ -13,3 +13,10 @@ female(michelle).
 
 sibling(X, Y) :- parent(Z, X), parent(Z, Y), X \= Y.
 brother(X, Y) :- male(X), sibling(X, Y).
+aunt(X, Y) :- female(X), sibling(X, P), parent(P, Y).
+cousin(X, Y) :- parent(P, X), parent(Q, Y), sibling(P, Q).
+grandparent(X,Y) :- parent(P,Y), parent(X,P).
+ancestor(X, Y) :- parent(X,Y).
+ancestor(X, Y) :- parent(P,Y), ancestor(X, P).
+descendent(X, Y) :- parent(Y,X).
+descendent(X, Y) :- parent(Y,P), descendent(X, P).
